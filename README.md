b423bd7cf1d584189fd1a10d55ea5e357437f471
Carl Bohme <bohmec@oregonstate.edu>
2023-04-28 21:44:59 -0700

    Added user account creation invitation system.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-Docker MySQL DB Creation
+## Docker MySQL DB Creation
 docker run --name osu-r6 -e MYSQL_ROOT_PASSWORD=password -d mysql
+
+
+## User Account Generation
+User accounts will only be able to be createed if that are provided a token from an administrator. This will prevent anyone from making an account. These tokens will be able to be creazted by admins on the site and stored in an 'invites' table in the database. Invites will be single use and mill be marked inactive after use. Using an invite token will also bind that account to that invite to create a log of which accounmt used which invite.
--- api/users.js
+++ api/users.js
@@ -1,70 +1,72 @@
-const router = require('express').Router();
+const router = require('express').Router()
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
 
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 var jsonParser = bodyParser.json() 
 
-const {
-    User,
-    UserSchema
-  } = require('../models/user');
+const { User, UserSchema } = require('../models/user');
 const { validateAgainstSchema } = require('../lib/validation');
+const { requireAuthentication, requireAdmin, generateAuthToken, generateInviteToken, requireInvite } = require('../lib/auth');
+
 
 /*
-* Create user
+* Register new user
+* Registering user requires admin generated invite
 */
-router.post('/', jsonParser, async (req, res, next) => {
+router.post('/', jsonParser, requireInvite, async (req, res, next) => {
     try {
-      schemaValidation = validateAgainstSchema(req.body, UserSchema);
+      schemaValidation = validateAgainstSchema(req.body, UserSchema)
       if(schemaValidation === null){
-        req.body.password = await bcrypt.hash(req.body.password, 8);
-        const newUser = await User.create(req.body);
+        req.body.password = await bcrypt.hash(req.body.password, 8)
+        const newUser = await User.create(req.body)
         res.status(201).send({
             newUser
-        });
+        })
       } else {
         res.status(400).send({
           error: schemaValidation
-      });
+      })
       }
     } catch (err) {
-      // Find a way to detemine if error 400 or 409
+      // TODO: Find a way to detemine if error 400 or 409 for proper error handling
       if(err.original.sqlMessage){
         res.status(400).send({
           error: err.original.sqlMessage
-        });
+        })
       } else {
         res.status(400).send({
           error: err
-        });
+        })
       }
     }
-  });
+  })
 
 
 /*
-* Login
+* Login user
 */
 router.post('/login', jsonParser, async(req, res, next) => {
   if(req.body.email && req.body.password){
     try {
-      const email = req.body.email;
-      const user = await User.findOne({ where: { email } });
+      const email = req.body.email
+      const user = await User.findOne({ where: { email } })
       if (!user) {
         res.status(400).send({
           error: "Invalid email or password"
         })
       }
-      const passwordMatches = await bcrypt.compare(req.body.password, user.password);
+      const passwordMatches = await bcrypt.compare(req.body.password, user.password)
       if (!passwordMatches) {
         res.status(400).send({
           error: "Invalid email or password"
         })
       }
       res.status(200).send({
-        success: "Logged in as " + user.name
-      });
+        success: "Logged in as " + user.name,
+        userId: user.id,
+        token: generateAuthToken(user.id)
+      })
     } catch (err) {
       res.status(500).send({
         error: "An error occurred while logging in"
@@ -73,6 +75,22 @@ router.post('/login', jsonParser, async(req, res, next) => {
   } else {
     res.status(400).send({
       error: "Login requires email and password"
+    })
+  }
+})
+
+
+/*
+* Create user invite token
+*/
+router.post('/invite', jsonParser, requireAuthentication, requireAdmin, async(req, res, next) => {
+  try{
+    res.status(200).send({
+      success: generateInviteToken(req.user)
+    })
+  } catch (err) {
+    res.status(500).send({
+      error: "Error generating invite"
     })
   }
 })
