3ad20282391a7d0a0766291edebdd21e9159e849
Carl Bohme <bohmec@oregonstate.edu>
2023-05-12 19:44:59 -0700

    Invites added as DB table

---
+++ models/invite.js
@@ -0,0 +1,28 @@
+const sequelize = require('../lib/sequelize')
+const {DataTypes } = require('sequelize')
+
+const {User} = require('../models/user')
+
+const Invite = sequelize.define('Invite', {
+    creator: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    status: {
+        type: DataTypes.ENUM('active', 'inactive'),
+        defaultValue: 'active'
+      },
+    token: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    usedBy: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    }
+  })
+
+  Invite.belongsTo(User, { foreignKey: 'creator' });
+  Invite.belongsTo(User, { foreignKey: 'usedBy' });
+
+  exports.Invite = Invite
